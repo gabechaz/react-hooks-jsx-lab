906c81dfa374b60461f2483624dc43c27fa81911
Gabriel Chazanov <gabe.chaz@gmail.com>
2021-02-01 23:09:03 -0500

    commit tree 12edf87c01f40535bd20d47157208113443696d0

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,12 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return <div>
+    <h2>About me
+      <p>Aint she nice</p>
+      <img src = {image} alt= 'I made this'></img>
+    </h2>
+  </div>;
 }
 
 export default About;
